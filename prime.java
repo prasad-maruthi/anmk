3a52ae10fb148a2e02fe3edd206a2f84ebcc5a5e
prasad-maruthi <prasadmaruthi409@gmail.com>
2025-11-09 12:17:41 +0530

    Add files via upload

---
+++ prime.java
@@ -0,0 +1,28 @@
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        System.out.println("Prime numbers between 1 and 100 are:");
+
+        for (int num = 2; num <= 100; num++) { // start from 2 since 1 is not prime
+            if (isPrime(num)) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+
+    // Function to check if a number is prime
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+
+        // Check divisibility up to the square root of the number
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false; // Not prime
+            }
+        }
+
+        return true; // Prime number
+    }
+}
+
